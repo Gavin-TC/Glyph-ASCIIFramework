f5dbdaf62412a4c21447ea2b744f6bc36b939d43
Gavin TC <gtclonts@gmail.com>
2023-07-06 18:19:11 -0400

    Map is now instanced into a buffer
    This makes it so that we aren't actually editing the map. Which now that I think about it could be the issue (TODO at RenderMap...)

--- Graphics/MapRenderer.cs
+++ Graphics/MapRenderer.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics.Tracing;
 using System.Formats.Asn1;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Threading.Channels;
 using Glyph_Game_Engine.Entities;
 using Glyph_Game_Engine.Exceptions;
 using Glyph_Game_Engine.Utilities;
@@ -9,23 +11,27 @@ public class MapRenderer
 {
     private readonly int screenWidth, screenHeight;
     // Screen buffer
-    private char[,]? buffer;
+    private char[,]? areaBuffer;
+    private char[,]? entityBuffer;
     private Map map { get; set; }
 
+    // Chars that are considered walls and therefore cannot be walked into.
     public List<char> wallChars = new List<char>();
+    // Chars that are considered walkable and therefore can be walked into.
+    public List<char> walkableChars = new List<char>();
 
-    public MapRenderer(int screenWidth, int screenHeight, Map map, List<char>? wallChars)
+    public MapRenderer(int screenWidth, int screenHeight, Map map, List<char>? wallChars, List<char>? walkableChars)
     {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
 
         this.map = map;
-        buffer = this.map.areaMap;
+        areaBuffer = this.map.areaMap;
+        entityBuffer = this.map.entityMap;
         
-        if (wallChars != null)
-        {
-            this.wallChars = wallChars;
-        }
+        if (wallChars != null) { this.wallChars = wallChars; }
+
+        if (walkableChars != null) { this.walkableChars = walkableChars; }
 
         // This hides the cursor and hides the 'refreshing' of the screen.
         Console.CursorVisible = false;
@@ -36,24 +42,40 @@ public class MapRenderer
         wallChars.Add(character);
     }
     
-    // This is done to dynamically change the map. 
     public void SetMap(Map mapObject)
     {
         this.map = mapObject;
     }
 
-    public void RenderMap()
+    // Currently, the Entity only has access to the areaMap as it's initialized.
+    // This means that the Entity doesn't see what's currently on the map, but what the map starts with.
+    // TODO: Make the Entity class able to check if the current instance of the map being rendered is a walkable tile.
+    public void RenderMap(List<Entity> entities)
     {
         if (map?.areaMap == null) { throw new UnsetValueException("Map has not been set."); }
-        
-        buffer = map.areaMap;
-        
+
         // This only renders the part of the map that fits within the screenWidth/Height.
         for (int y = 0; y < screenHeight; y++)
         {
+            
             for (int x = 0; x < screenWidth; x++)
             {
-                Console.Write(buffer[y, x]);
+                bool entityRendered = false;
+
+                foreach (var entity in entities)
+                {
+                    if (entity.x == x && entity.y == y)
+                    {
+                        Console.Write(entity.symbol);
+                        entityRendered = true;
+                        break; // Exit the loop once an entity is rendered for this tile
+                    }
+                }
+
+                if (!entityRendered)
+                {
+                    Console.Write(areaBuffer[y, x]);
+                };
             }
             Console.WriteLine();
         }
@@ -64,6 +86,8 @@ public class MapRenderer
         if (map?.entityMap == null || map?.areaMap == null) { throw new UnsetValueException("Map has not been set."); }
         if (entities == null) { throw new UnsetValueException("Entities list is null."); }
 
+        entityBuffer = map.entityMap;
+
         // This only renders the entities in the visible screen.
         for (int y = 0; y < screenHeight; y++)
         {
@@ -71,18 +95,15 @@ public class MapRenderer
             {
                 foreach (var entity in entities)
                 {
+                    // If there's an entity within the screen, print it at its location.
                     if (entity.x == x && entity.y == y)
                     {
-                        map.entityMap[y, x] = entity.symbol;
-                    }
-                    else
-                    {
-                        if (!wallChars.Contains(map.entityMap[y, x]))
-                        {
-                            map.entityMap[y, x] = ' ';
-                        }
+                        entityBuffer[y, x] = entity.symbol;
                     }
                 }
+
+                Console.WriteLine("-ENTITY MAP: " + map.entityMap[y, x] + "-");
+                entityBuffer[y, x] = map.entityMap[y, x];
             }
         }
     }
